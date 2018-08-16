04e5cbcea28338de706424eb6c5b7933afa4651a
Mike Michaud <michaud.michael@gmail.com>
2018-08-16 15:58:08 -0700

    count does not increase automatically without page refresh

--- client/src/components/Invite.js
+++ client/src/components/Invite.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import {update, getUsers} from '../actions/actions'
+import {update, getUsers, getGoing, getNotgoing} from '../actions/actions'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
 class Invite extends Component {
+  state = {
+    status: []
+  }
  componentDidMount() {
 		getUsers()
+    getGoing()
+    getNotgoing()
 	}
 //need componentwillreceiveprops
 	updateGoing = (id) => {
@@ -19,9 +24,9 @@ class Invite extends Component {
     return (
       <div className="container">
       	<div className="stats">
-	      <Link to='/going'>Going: {this.props.going.length}</Link>
-    	  <Link to='/notgoing'>Not Going: {this.props.notgoing.length}</Link>
-    	</div>
+	         <Link to='/going'>Going: {this.props.going.length}</Link>
+    	     <Link to='/notgoing'>Not Going: {this.props.notgoing.length}</Link>
+    	  </div>
         {this.props.users.map(user => (
         	<div className="card" key={user.id}>
         		<img src={user.image} alt={user.name}/>
@@ -30,8 +35,8 @@ class Invite extends Component {
         			<p>Email: {user.email}</p>
         			<p>Phone: {user.phone}</p>
         		</div>
-        		<button  type="submit" className="gButton" onClick={() => {this.updateGoing(user.id)}}><i className="fa fa-check"></i></button>
-        		<button type="submit" className="ngButton" onClick={() => {this.updateNotGoing(user.id)}}><i className="fa fa-times"></i></button>
+        	   	<button  type="submit" className="gButton" onClick={() => {this.updateGoing(user.id)}}><i className="fa fa-check"></i></button>
+        		  <button type="submit" className="ngButton" onClick={() => {this.updateNotGoing(user.id)}}><i className="fa fa-times"></i></button>
         	</div>
         ))}
       </div>
